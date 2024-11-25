6264e015ffc447d3ce3293dc1bbd52905123ed50
Godflamejava <58296857+Godflamejava@users.noreply.github.com>
2024-11-25 21:21:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Countdown from "./components/Countdown";
+
+const App = () => {
+  return <Countdown />;
+};
+export default App;
